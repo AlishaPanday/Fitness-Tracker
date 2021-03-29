d50e6b6348c1c344ecaaf9093c11e48528f13021
Alisha Panday <panday.alisha90@gmail.com>
2021-03-29 20:59:24 +0800

    Fixed total duration

--- database/connect.js
+++ database/connect.js
@@ -1,12 +1,16 @@
 const mongoose = require('mongoose');
 require('dotenv').config()
 function connect() {
-    mongoose.connect(process.env.MONGODB_Fitness, {
+    const connection = mongoose.connect(process.env.MONGODB_Fitness, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
         useCreateIndex: true,
         useUnifiedTopology: true,
     });
+
+    connection.catch(err => {
+        console.log('err ' + err);
+    })
 }
 module.exports= {
     connect,
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,13 +2,34 @@ const db = require("../models");
 const express = require("express");
 const router = express.Router();
 
-router.get("/api/Workouts", (req, res) => {
+router.get("/api/workouts", (req, res) => {
   db.Workout.find({})
-    .then(dbWorkout =>  { res.json(dbWorkout); })
-    .catch(err => { res.json(err); });
+    .then(dbWorkouts =>  { 
+      console.log(dbWorkouts);
+      const workouts = dbWorkouts.map(workout =>{
+        console.log({workout});
+        const duration = workout.exercises.reduce((acc, next)=>{
+          return acc + next.duration ;
+        }, 0);
+
+        return {
+          totalDuration: duration,
+          ...workout.toObject()
+        }
+
+      })
+      
+
+      res.json(workouts); 
+    })
+    .catch(err => { 
+      console.log(err);
+      res.json(err); 
+    });
+
 });
 
-router.put("/api/Workouts/:id", (req, res) => {
+router.put("/api/workouts/:id", (req, res) => {
   console.log(req.body)
   db.Workout.findByIdAndUpdate(
     req.params.id,
@@ -18,13 +39,13 @@ router.put("/api/Workouts/:id", (req, res) => {
     .catch(err => { res.json(err); });
 });
 
-router.post("/api/Workouts", ({ body }, res) => {
+router.post("/api/workouts", ({ body }, res) => {
   db.Workout.create(body)
     .then(dbWorkout => { res.json(dbWorkout); })
     .catch(err => { res.json(err); });
 });
 
-router.get("/api/Workouts/range", (req, res) => {
+router.get("/api/workouts/range", (req, res) => {
   db.Workout.find({})
     .then(dbWorkout => { res.json(dbWorkout); })
     .catch(err => { res.json(err); });
